1e71129acb04efb306c7613a3c211b16e0ff125f
walacealves <walace.alves10@gmail.com>
2023-03-07 13:28:10 -0300

    git init

---
+++ src/main/java/com/acch/votacaofestivalcancao/VotacaofestivalcancaoApplication.java
@@ -0,0 +1,13 @@
+package com.acch.votacaofestivalcancao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotacaofestivalcancaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VotacaofestivalcancaoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/acch/votacaofestivalcancao/VotacaofestivalcancaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acch.votacaofestivalcancao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotacaofestivalcancaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
